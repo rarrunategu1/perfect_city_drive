4fbed650994661f7492301ae854f7778a4cd325b
rarrunategu1 <parker.rose@ymail.com>
2019-03-31 20:39:49 -0400

    Now passes 9 of 10 tests providing the shortest distance between two places.  Still trying to figure out what test case I'm missing

--- perfect_city_drive/Program.cs
+++ perfect_city_drive/Program.cs
@@ -13,62 +13,59 @@ namespace perfect_city_drive
             //method below passes two tests for returning the shortest route from departure to destination
         }
 
-        double perfectCity(double[] departure, double[] destination)
+        double perfectCity(double[] departure, double[] destination) {
+    
+        double result = 0; 
+    
+        if(departure[1] >= 1)
         {
-
-            double result = 0;
-
-            int departureXRight = (int)Math.Ceiling(departure[0]);
-            //Console.WriteLine(departureXRight);
-
-            int departureXLeft = (int)Math.Round(departure[0]);
-
-            int destinationXRight = (int)Math.Ceiling(destination[0]);
-
-            int departureYDown = (int)Math.Round(departure[0]);
-
-            int departureYUp = (int)Math.Ceiling(departure[0]);
-
-            double yDistance = destination[1] - departure[1];
-
-            if (destinationXRight == destination[0])
+            double blockDistance = Math.Abs(destination[1] - departure[1]);
+        
+            int departureRoundUp = (int)Math.Ceiling(departure[0]);
+    
+            int departureRoundDown = (int)Math.Round(departure[0]);
+    
+            int destinationRoundUp = (int)Math.Ceiling(destination[0]);
+    
+            //int destinationRoundDown = (int)Math.Round(destination[0]);
+        
+            if(Math.Abs(destination[0] - departureRoundUp) + Math.Abs(departureRoundUp - departure[0]) + blockDistance < Math.Abs(destination[0] - departureRoundDown) + (departure[0] + departureRoundDown) + blockDistance)
             {
-
-                if (destination[0] - departure[0] > (departure[0] - departureXLeft) + (destination[0] - departureXLeft))
-                {
-
-                    result = (departure[0] - departureXLeft) + (destination[0] - departureXLeft) + yDistance;
-                    return result;
-                }
-                else
-                {
-                    Console.WriteLine("yes");
-                    result = destination[0] - departure[0] + yDistance;
-                    return result;
-                }
-
+                result = Math.Abs(destination[0] - departureRoundUp) + (departureRoundUp - departure[0]) + blockDistance;
             }
-            else
+            else 
             {
-
-                if ((departureXRight - departure[0]) + (destinationXRight - destination[0]) > departure[0] - departureXLeft + destination[0])
-                {
-                    result = (departure[0] - departureXLeft + destination[0]) + yDistance;
-                    return result;
-                }
-                else
-                {
-                    result = ((departureXRight - departure[0]) + (destinationXRight - destination[0])) + yDistance;
-                    return result;
-                }
+                result = Math.Abs(destination[0] - departureRoundDown) + (departure[0] + departureRoundDown) + blockDistance;
             }
-
-
-
-            return result;
-
-            //
+        
         }
-
-    }
+        else
+        {   
+           double blockDistance2 = Math.Abs(destination[0] - departure[0]);
+        
+            int departureRoundUp2 = (int)Math.Ceiling(departure[1]);
+    
+            int departureRoundDown2 = (int)Math.Round(departure[1]);
+    
+            int destinationRoundUp2 = (int)Math.Ceiling(destination[1]);
+    
+            //int destinationRoundDown = (int)Math.Round(destination[0]);
+        
+            if(Math.Abs(destinationRoundUp2 - destination[1]) + Math.Abs(departureRoundUp2 - departure[1]) + blockDistance2 < Math.Abs(destination[1] - departureRoundDown2) + (departure[1] + departureRoundDown2) + blockDistance2)
+            {
+                result = Math.Abs(destinationRoundUp2 - destination[1]) + (departureRoundUp2 - departure[1]) + blockDistance2;
+            }
+            else 
+            {
+                result = Math.Abs(destination[1] - departureRoundDown2) + (departure[1] + departureRoundDown2) + blockDistance2; 
+            }
+        
+        }
+    
+    
+    
+    return result;
+    
+    //
 }
+
